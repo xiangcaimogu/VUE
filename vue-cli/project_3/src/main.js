9c41b5b5aa18e0c30fcf67010a71b2d3a516ff14
zhuchuanhuan <18352613036@163.com>
2019-04-02 14:07:16 +0800

    2

--- vue-cli/project_3/src/main.js
+++ vue-cli/project_3/src/main.js
@@ -8,46 +8,44 @@ import router from './router'
 import MintUI from 'mint-ui'
 // 引入css，不用对象直接引用
 import 'mint-ui/lib/style.css'
-
-//图片插件
+// 配置axios
+import Axios from 'axios'
+// 图片插件
 import VuePreview from 'vue-preview'
-Vue.use(VuePreview)
-Vue.component(vue-preview,VuePreview)
-// 引入自己的CSS
-import "./assets/css/global.css"
+// 定义mement全局日期过滤器
+import Moment from 'moment'
 // 引入自己的ul和li组件
 import Myul from '@/components/common/myul'
 import Myli from '@/components/common/myli'
 import NavBar from '@/components/common/NavBar'
 import Comment from '@/components/common/comment'
-
+// 引入自己的CSS
+import './assets/css/global.css'
 // 注册全局组件
-Vue.component(Myul.name,Myul)
-Vue.component(Myli.name,Myli)
-Vue.component(NavBar.name,NavBar)
-Vue.component(Comment.name,Comment)
-
-// 定义mement全局日期过滤器
-import Moment from 'moment'
-Vue.filter('converTime',function (data,formatStr) {
+Vue.component(Myul.name, Myul)
+Vue.component(Myli.name, Myli)
+Vue.component(NavBar.name, NavBar)
+Vue.component(Comment.name, Comment)
+Vue.filter('converTime', function (data, formatStr) {
   return Moment(data).format(formatStr)
 })
-// 配置axios
-import Axios from 'axios'
+Vue.use(VuePreview)
+Vue.component('vue-preview', VuePreview)
+
 // 配置公共URL
 Axios.defaults.baseURL = 'http://www.sinya.online/api/'
 Vue.prototype.$axios = Axios
 
-//配置请求拦截器
+// 配置请求拦截器
 Axios.interceptors.request.use(function (config) {
   MintUI.Indicator.open({
     text: '玩命加载...'
   })
   return config
 })
-//配置相应拦截器
+// 配置相应拦截器
 Axios.interceptors.response.use(function (res) {
-  MintUI.Indicator.close();
+  MintUI.Indicator.close()
   return res
 })
 // 安装UI插件，注册全局组件及挂载组件
